7026a92b68903db5dd9775599bcc81006adf886a
FindingNimoy <josh.faber@wavetronix.com>
2022-09-15 11:22:08 -0600

    Got TinyGPS working!

--- gps arduino/test_gps.ino
+++ gps arduino/test_gps.ino
@@ -1,5 +1,14 @@
 #include <TinyGPSPlus.h>
 #include <TinyGPS++.h>
+//to get TinyGPS to work, I needed to comment out WProgram.h and change some of the int references
+//typedef __int8 int8_t;
+//typedef unsigned __int8 uint8_t;
+//typedef __int16 int16_t;
+//typedef unsigned __int16 uint16_t;
+//typedef __int32 int32_t;
+//typedef unsigned __int32 uint32_t;
+
+//apparently the library uses a newer version of c++, which is not super compatible with our board
 
 #include <Arduino.h>
 
@@ -8,19 +17,31 @@
 
 //SERCOM sercom5(sercom5);
 //TwoWire myWire(&sercom5, 0, 1);
+TinyGPSPlus gps;
 
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_BUILTIN, OUTPUT);
-  TinyGPSPlus gps;
-  //myWire.begin();
-
+  Serial1.begin(9600);
+  Serial.begin(115200);
 }
 
 // the loop function runs over and over again forever
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(5000);                       // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  delay(5000);                       // wait for a second
+  while(Serial1.available() > 0)
+  {
+    //gps.encode(Serial1.read());
+   gps.encode(Serial1.read());
+  }
+
+  if (gps.location.isUpdated())
+  {
+    Serial.print("Lat: "); Serial.println(gps.location.lat(), 9);
+    Serial.print("Long: "); Serial.println(gps.location.lng(), 9);
+  }
+    
+  if (gps.satellites.isUpdated())
+  {
+    Serial.print("Sat: "); Serial.println(gps.satellites.value());
+  }  
 }
