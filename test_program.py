401a0cd8858f825123991710cf227c160e0dbbcb
FindingNimoy <josh.faber@wavetronix.com>
2022-09-29 12:00:10 -0600

    Script fixes, changing poll rate from 10-5 seconds

--- test_program.py
+++ test_program.py
@@ -31,11 +31,11 @@ if __name__ == "__main__":
 
     drone = mv.movement()       
     drone_var = drone.get_drone()
-    start_time = drone_var.get_flight_time()
+    #start_time = drone_var.get_flight_time()
 
 
     print(">>>>>>>>>>>>>>>>START BATTERY: ", drone_var.get_battery(), "%")
-    print(">>>>>>>>>>>>>>>>BEGINNING FLIGHT TIME: ", start_time)
+    print(">>>>>>>>>>>>>>>>BEGINNING FLIGHT TIME: ", drone_var.get_flight_time())
 
     row = [0, 0]
 
@@ -48,8 +48,8 @@ if __name__ == "__main__":
         try:
             drone.move(up=20)
         except:
-            f.close()
             writer.writerow("Flight Terminated Early")
+            f.close()
             print(">>>>>>>>>>>>>>>>CRITICAL: Drone terminated early, writing data to file", filename)
         current_time = drone_var.get_flight_time()
         current_bat = drone_var.get_battery()
@@ -58,13 +58,22 @@ if __name__ == "__main__":
         print("Time: ", current_time)
         print("Battery: ", current_bat, "%")
         writer.writerow(row)
-        sleep(10)
+        sleep(5)
+
+        current_time = drone_var.get_flight_time()
+        current_bat = drone_var.get_battery()
+        row[0] = current_time
+        row[1] = current_bat
+        print("Time: ", current_time)
+        print("Battery: ", current_bat, "%")
+        writer.writerow(row)
+        sleep(5)
 
         try:
             drone.move(down=20)
         except:
-            f.close()
             writer.writerow("Flight Terminated Early")
+            f.close()
             print(">>>>>>>>>>>>>>>>CRITICAL: Drone terminated early, writing data to file ", filename)
         current_time = drone_var.get_flight_time()
         current_bat = drone_var.get_battery()
@@ -73,19 +82,29 @@ if __name__ == "__main__":
         print("Time: ", current_time)
         print("Battery: ", current_bat, "%")
         writer.writerow(row)
-        sleep(10)
+        sleep(5)
+
+        current_time = drone_var.get_flight_time()
+        current_bat = drone_var.get_battery()
+        row[0] = current_time
+        row[1] = current_bat
+        print("Time: ", current_time)
+        print("Battery: ", current_bat, "%")
+        writer.writerow(row)
+        sleep(5)
 
     end_time = drone_var.get_flight_time()
     print(">>>>>>>>>>>>>>>>ENDING FLIGHT TIME: ", end_time)
 
-    total_time = end_time - start_time
-    total_minutes = floor(total_time / 60)
-    total_seconds = total_time % 60
+    total_minutes = floor(end_time / 60)
+    total_seconds = end_time % 60
 
     time_string = "Total Flight Time: " + str(total_minutes) + " minutes " + str(total_seconds) + " seconds"
     print(time_string)
     writer.writerow(time_string)
     writer.writerow("Successful Flight!")
+
+    print(">>>>>>>>>>>>>>>>SUCCESS: Writing data to file ", filename)
         
     # close out of .csv file
     f.close()
